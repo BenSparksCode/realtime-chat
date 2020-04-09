8bc18ee033b82f1314a4be13005a362d36c6a7db
Ben Sparks <bensparks0@gmail.com>
2020-04-10 01:45:10 +0200

    Added Socket.io to client, created Users CRUD functions on server

--- client/src/components/Chat/Chat.js
+++ client/src/components/Chat/Chat.js
@@ -1,10 +1,40 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import queryString from 'query-string'
+import io from 'socket.io-client'
 
 
 import './Chat.css'
 
+let socket;
 
-const Chat = () => {
+const Chat = ({location}) => {
+
+    const [name, setName] = useState('')
+    const [room, setRoom] = useState('')
+    const ENDPOINT = 'localhost:5000'
+
+    //componentDidRender
+    useEffect(() => {
+        const {name, room} = queryString.parse(location.search)
+
+        socket = io(ENDPOINT)
+
+        setName(name)
+        setRoom(room)
+
+        socket.emit('join', {name, room}, () => {
+            //callback fn passed to server goes here
+        })
+
+        return () => {
+            socket.emit('disconnect')
+            socket.off()
+        }
+
+    }, [ENDPOINT, location.search])
+
+
+
     return (
         <div>
             <h1>Chat</h1>
--- server/index.js
+++ server/index.js
@@ -2,6 +2,8 @@ const express = require('express')
 const socketio = require('socket.io')
 const http = require('http')
 
+const { addUser, removeUser, getUser, getUsersInRoom } = require('./users')
+
 const PORT = process.env.PORT || 5000
 
 const router = require('./router')
@@ -12,6 +14,10 @@ const io = socketio(server)
 
 io.on('connection', (socket) => {
     console.log("New connection made!");
+
+    socket.on('join', ({name, room}, callback) => {
+        console.log(name, room);
+    })
 
     socket.on('disconnect', () => {
         console.log("User just left!");
