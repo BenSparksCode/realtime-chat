098398fb38f5da7c13158f6b232ac15ed83d207e
Ben Sparks <bensparks0@gmail.com>
2020-04-10 14:09:38 +0200

    Added basic input and logic to test messages sent/recieved on frontend

--- client/src/components/Chat/Chat.js
+++ client/src/components/Chat/Chat.js
@@ -2,27 +2,29 @@ import React, { useState, useEffect } from 'react'
 import queryString from 'query-string'
 import io from 'socket.io-client'
 
-
 import './Chat.css'
 
 let socket;
 
-const Chat = ({location}) => {
+const Chat = ({ location }) => {
 
     const [name, setName] = useState('')
     const [room, setRoom] = useState('')
+    const [message, setMessage] = useState('')
+    const [messages, setMessages] = useState([])
+
     const ENDPOINT = 'localhost:5000'
 
-    //componentDidRender
+    //For users joining/leaving
     useEffect(() => {
-        const {name, room} = queryString.parse(location.search)
+        const { name, room } = queryString.parse(location.search)
 
         socket = io(ENDPOINT)
 
         setName(name)
         setRoom(room)
 
-        socket.emit('join', {name, room}, () => {
+        socket.emit('join', { name, room }, () => {
             //callback fn passed to server goes here
         })
 
@@ -33,12 +35,31 @@ const Chat = ({location}) => {
 
     }, [ENDPOINT, location.search])
 
+    //For handling new messages received on backend
+    useEffect(() => {
+        socket.on('message', (message) => {
+            //add new messages to array in state as received
+            setMessages([...messages, message])
+        })
+    }, [messages])
 
+
+    //TODO: add function for sending messages
 
     return (
-        <div>
+        <div className='outerContainer'>
             <h1>Chat</h1>
-            
+            <div className='container'>
+                <input
+                    type="text"
+                    value={message}
+                    onChange={e => {
+                        setMessage(e.target.value)
+                    }}
+                    onKeyPress={e => {
+                        if (e.key === 'Enter') sendMessage(e)
+                    }} />
+            </div>
         </div>
     )
 }
